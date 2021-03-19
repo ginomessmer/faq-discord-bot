d4aa1988c567bcda488a37b5d4c40930d44ff031
Gino Messmer <gino@messmerized.de>
2021-03-19 20:37:22 +0100

    Add namespace

--- QnaMakerDiscordBot/Azure/QnaMakerAnswerResponse.cs
+++ QnaMakerDiscordBot/Azure/QnaMakerAnswerResponse.cs
@@ -1,11 +1,14 @@
-﻿public class QnaMakerAnswerResponse
+﻿namespace QnaMakerDiscordBot.Azure
 {
-    public object[] Questions { get; set; }
-    public string Answer { get; set; }
-    public float Score { get; set; }
-    public int Id { get; set; }
-    public bool IsDocumentText { get; set; }
-    public object[] Metadata { get; set; }
+    public class QnaMakerAnswerResponse
+    {
+        public object[] Questions { get; set; }
+        public string Answer { get; set; }
+        public float Score { get; set; }
+        public int Id { get; set; }
+        public bool IsDocumentText { get; set; }
+        public object[] Metadata { get; set; }
     
-    public override string ToString() => Answer;
+        public override string ToString() => Answer;
+    }
 }

fc386c08afde47116b6d1fc854a3e166c7bc19db
Gino Messmer <gino@messmerized.de>
2021-05-06 21:31:19 +0200

    Refactoring

--- src/FaqDiscordBot/Workers/DanglingQuestionsReminderWorker.cs
+++ src/FaqDiscordBot/Workers/DanglingQuestionsReminderWorker.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -7,6 +8,7 @@ using Discord;
 using Discord.WebSocket;
 using FaqDiscordBot.Events;
 using FaqDiscordBot.Infrastructure;
+using FaqDiscordBot.Models;
 using FaqDiscordBot.Options;
 using FaqDiscordBot.Properties;
 using MediatR;
@@ -47,12 +49,7 @@ namespace FaqDiscordBot.Workers
                     await using var dbContext = scope.ServiceProvider.GetRequiredService<FaqDbContext>();
                     using var discordClient = scope.ServiceProvider.GetRequiredService<IDiscordClient>();
 
-                    var questions = await dbContext.Questions.AsQueryable()
-                        .Where(x => x.Meta.ReminderAt <= DateTime.UtcNow
-                                    && x.Answer == null
-                                    && x.UserId != 0
-                                    && x.CreatedAt + _options.PurgeThreshold >= DateTime.UtcNow)
-                        .ToListAsync(stoppingToken);
+                    var questions = await GetDanglingQuestionsAsync(dbContext, stoppingToken);
 
                     foreach (var question in questions)
                     {
@@ -62,20 +59,7 @@ namespace FaqDiscordBot.Workers
                             if (user is null)
                                 continue;
 
-                            var dmChannel = await user.GetOrCreateDMChannelAsync();
-                            var message = await dmChannel.GetMessageAsync(question.Meta.MessageId);
-
-                            await dmChannel.SendMessageAsync(embed: new EmbedBuilder()
-                                .WithTitle(Resources.UnansweredQuestionReminderEmbed_Title)
-                                .WithDescription(
-                                    new StringBuilder()
-                                        .AppendLine($"> {question}")
-                                        .AppendLine($"> _{message.GetJumpUrl()}_")
-                                        .AppendLine()
-                                        .AppendLine(Resources.UnansweredQuestionReminderEmbed_Desc)
-                                        .ToString())
-                                .WithColor(Color.Orange)
-                                .Build());
+                            await SendReminderDirectMessageAsync(user, question);
                         }
                         catch (Exception ex)
                         {
@@ -87,11 +71,39 @@ namespace FaqDiscordBot.Workers
                 }
                 catch (Exception ex)
                 {
-                    _logger.LogError(ex, "Couldn't send reminder. Trying again later...");
+                    _logger.LogError(ex, "Couldn't send pending reminders. Trying again later...");
                 }
 
                 await Task.Delay(TimeSpan.FromHours(1), stoppingToken);
             }
+        }
+
+        private async Task<List<Question>> GetDanglingQuestionsAsync(FaqDbContext dbContext, CancellationToken stoppingToken)
+        {
+            return await dbContext.Questions.AsQueryable()
+                .Where(x => x.Meta.ReminderAt <= DateTime.UtcNow
+                            && x.Answer == null
+                            && x.UserId != 0
+                            && x.CreatedAt + _options.PurgeThreshold >= DateTime.UtcNow)
+                .ToListAsync(stoppingToken);
+        }
+
+        private static async Task SendReminderDirectMessageAsync(IUser user, Question question)
+        {
+            var dmChannel = await user.GetOrCreateDMChannelAsync();
+            var message = await dmChannel.GetMessageAsync(question.Meta.MessageId);
+
+            await dmChannel.SendMessageAsync(embed: new EmbedBuilder()
+                .WithTitle(Resources.UnansweredQuestionReminderEmbed_Title)
+                .WithDescription(
+                    new StringBuilder()
+                        .AppendLine($"> {question}")
+                        .AppendLine($"> _{message.GetJumpUrl()}_")
+                        .AppendLine()
+                        .AppendLine(Resources.UnansweredQuestionReminderEmbed_Desc)
+                        .ToString())
+                .WithColor(Color.Orange)
+                .Build());
         }
     }
 }

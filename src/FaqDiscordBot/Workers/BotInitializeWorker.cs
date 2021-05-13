abaf9a0c9d261716e2c2430ea8459a30b9c2374f
Gino Messmer <8465892+ginomessmer@users.noreply.github.com>
2021-05-13 14:59:34 +0200

    Fix: Discord client disconnect on startup (#16)
    * Add logs to Discord client
    
    * Fix multiple Discord clients interference

--- src/FaqDiscordBot/Workers/BotInitializeWorker.cs
+++ src/FaqDiscordBot/Workers/BotInitializeWorker.cs
@@ -2,6 +2,8 @@ using System;
 using System.Globalization;
 using System.Threading;
 using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
 using FaqDiscordBot.Options;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -11,18 +13,24 @@ namespace FaqDiscordBot.Workers
 {
     public class BotInitializeWorker : BackgroundService
     {
+        private readonly DiscordSocketClient _client;
         private readonly ILogger<BotInitializeWorker> _logger;
+        private readonly ILogger<DiscordSocketClient> _discordLogger;
         private readonly BotOptions _options;
 
-        public BotInitializeWorker(IOptions<BotOptions> options, ILogger<BotInitializeWorker> logger)
+        public BotInitializeWorker(DiscordSocketClient client, IOptions<BotOptions> options,
+            ILogger<BotInitializeWorker> logger, ILogger<DiscordSocketClient> discordLogger)
         {
+            _client = client;
             _logger = logger;
+            _discordLogger = discordLogger;
             _options = options.Value;
         }
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
             ApplyCulture();
+            ApplyLogger(stoppingToken);
 
             return Task.CompletedTask;
         }
@@ -41,6 +49,20 @@ namespace FaqDiscordBot.Workers
             {
                 _logger.LogInformation(ex, "Failed to set culture to {CultureName}", _options.CultureName);
             }
+        }
+
+        private void ApplyLogger(CancellationToken stoppingToken)
+        {
+            _client.Log += message => Task.Run(() => _discordLogger.Log(message.Severity switch
+            {
+                LogSeverity.Critical => LogLevel.Critical,
+                LogSeverity.Debug => LogLevel.Debug,
+                LogSeverity.Error => LogLevel.Error,
+                LogSeverity.Info => LogLevel.Information,
+                LogSeverity.Verbose => LogLevel.Trace,
+                LogSeverity.Warning => LogLevel.Warning,
+                _ => LogLevel.Trace
+            }, message.Exception, message.Message), stoppingToken);
         }
     }
 }
